219c7b3c7861e260d1a4c163ce8f6c00ac6b6cd6
bronbonsma <babonsma@gmail.com>
2020-04-19 22:47:42 +0300

    Ex4

--- Filehandler.py
+++ Filehandler.py
@@ -1,11 +1,12 @@
 import csv
+from csv import writer
 
 class FileHandler:
 
-    def load_from_function(self, file_name):
+    def load_from_cvs(self, file_name):
         try:
             with open(file_name, 'r') as csv_file:
-                csv_reader = csv.reader(csv_file)
+                csv_reader = csv.DictReader(csv_file)
 
                 for line in csv_reader:
                     print(line)
@@ -14,10 +15,28 @@ class FileHandler:
         except Exception as error:
             print("There is an error :" + str(error))
 
+    def append_to_csv(self, file_name, data_input):
+        try:
+            with open(file_name, 'a+', newline='') as write_obj:
+                csv_writer = writer(write_obj)
+                csv_writer.writerow(data_input)
 
+                for row in csv_writer:
+                    print(row[0])
+
+        except Exception as error:
+            print("There is an error:" + str(error))
+
+
+data_input = [14, 'Tom', 'Jerry', 'password', 'student', 100, 'teacher']
 
 file = FileHandler()
-file.load_from_function("/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv")
+file.append_to_csv("/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv",data_input)
+file.load_from_cvs("/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv")
+
+
+
+
 
 
 
