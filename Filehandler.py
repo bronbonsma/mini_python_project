71431c7c6787049c9a7aa76601b2e45d1225e9cf
bronbonsma <babonsma@gmail.com>
2020-04-20 19:15:23 +0300

    Day1

--- Filehandler.py
+++ Filehandler.py
@@ -1,38 +1,113 @@
 import csv
 from csv import writer
 
-class FileHandler:
 
-    def load_from_cvs(self, file_name):
+class File_handler:
+
+    def __init__(self,):
+        self.data = []
+
+    def load_from_csv(self, file_name):
         try:
             with open(file_name, 'r') as csv_file:
                 csv_reader = csv.DictReader(csv_file)
 
                 for line in csv_reader:
-                    print(line)
-
+                    self.data.append(line)
 
         except Exception as error:
             print("There is an error :" + str(error))
 
-    def append_to_csv(self, file_name, data_input):
+    def append_to_csv(self, file_name, data):
         try:
+
+            self.load_from_csv(file_name)
+            for row in self.data:
+                if row.get("id") == data[0]:
+                    raise Exception("This ID already exists")
+
             with open(file_name, 'a+', newline='') as write_obj:
                 csv_writer = writer(write_obj)
-                csv_writer.writerow(data_input)
-
-                for row in csv_writer:
-                    print(row[0])
+                csv_writer.writerow(data)
 
         except Exception as error:
-            print("There is an error:" + str(error))
+            print("There is an error :" + str(error))
 
 
-data_input = [14, 'Tom', 'Jerry', 'password', 'student', 100, 'teacher']
+# data_input = ['24', 'Tom', 'Knecht', 'password', 'student', 100, 'teacher']
+#
+# file = File_handler()
+# file.append_to_csv("/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv", data_input)
+# file.load_from_csv("/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv")
 
-file = FileHandler()
-file.append_to_csv("/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv",data_input)
-file.load_from_cvs("/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import csv
+# from csv import writer
+#
+# class FileHandler:
+#
+#     def load_from_csv(self, file_name):
+#
+#         try:
+#             with open(file_name, 'r') as csv_file:
+#                 csv_reader = csv.DictReader(csv_file)
+#
+#                 for line in csv_reader:
+#                     print(line)
+#
+#
+#         except Exception as error:
+#             print("There is an error :" + str(error))
+#
+#     def append_to_csv(self, file_name, data_input):
+#         try:
+#
+#             with open(file_name, 'r') as csv_file:
+#                 csv_reader = csv.DictReader(csv_file)
+#
+#
+#
+#             with open(file_name, 'a+', newline='') as write_obj:
+#                 csv_writer = writer(write_obj)
+#                 csv_writer.writerow(data_input)
+#
+#                 for row in csv_writer:
+#                     print(row)
+#
+#         except Exception as error:
+#             print("There is an error:" + str(error))
+#
+#
+# data_input = [15, 'Jack', 'Jerry', 'password', 'student', 100, 'teacher']
+#
+# file = FileHandler()
+# file.append_to_csv("/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv", data_input)
+# file.load_from_csv("/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv")
 
 
 
--- User.py
+++ User.py
@@ -1,35 +1,52 @@
-import csv
+
+from Filehandler import File_handler
+
 
 class User:
+    def __init__(self):
+        self.handler = File_handler()
+
+    def user_auth(self, username, password):
+        self.handler.load_from_csv('/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv')
+        print(self.handler.data)
+
+        access = False
+        user_role = ' '
+
+        for row in self.handler.data:
+         if username == row["first_name"] and password == row["password"]:
+            access = True
+            user_role = str(row["role"])
+
+         if access:
+            print('Access Granted')
+            print(f'Your role is {user_role}')
+            return access
+
+         else:
+            print('Access Denied')
+            return access
+
+User.__init__(User)
+User.user_auth(User, "amir", '12345678')
 
 
-    def _init__(self):
-        self.first_name = str(input('Please insert your username: '))
-        self.password = str(input('Please insert your password: '))
 
 
 
-    def user_auth(first_name, password):
-        with open('/Users/macpro/PycharmProjects/mini_python_project/cvs.file/user.csv') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            access = False
-            user_role = ' '
-            for row in csv_reader:
-                if first_name.lower() == row[1] and password.lower() == row[3]:
-                    access = True
-                    user_role = str(row[6])
 
 
-            if access:
-                print('Access Granted')
-                print(f'Your role is {user_role}')
-                return access
-            else:
-                print('Access Denied')
-                return access
 
 
-User._init__(User)
-User.user_auth(User.first_name, User.password)
+
+
+
+
+
+
+
+
+
+
 
 
